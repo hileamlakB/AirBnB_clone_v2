78cf7eee6fb96d9e1a85a59b5eb48d54c7606bf3
= <=>
2021-03-31 22:29:45 +0200

    remove the if condition

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,7 +18,7 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls is None or FileStorage.__objects is None:
+        if cls is None:
             return FileStorage.__objects
         else:
             returned_dic = {}
@@ -46,8 +46,6 @@ class FileStorage:
             class_obj_ref = type(obj).__name__ + '.' + obj.id
             if class_obj_ref in FileStorage.__objects.keys():
                 del self.__objects[class_obj_ref]
-        else:
-            return
 
     def reload(self):
         """Loads storage dictionary from file"""

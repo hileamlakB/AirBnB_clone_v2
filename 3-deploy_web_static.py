6efa71dedeb6cdcbe3f753cdd7cd7ad46d45f113
hileamlakb <hilea.buisness@gmail.com>
2021-04-15 16:40:47 +0300

    Add handdler for non existinf folder

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -41,35 +41,34 @@ def do_deploy(archive_path):
     """Deploys a achive to a number of remote hosts
     """
 
-    try:
-        local("test -e {} && echo True ".format(archive_path), capture=True)
-    except Exception:
+    with settings(warn_only=True):
+        path = local("test -e {} && echo True ".format(archive_path), capture=True)
+    if not path:
         return False
-    else:
 
-        file_name = archive_path.split("/")[-1]
-        folder_name = file_name.split(".")[0]
+    file_name = archive_path.split("/")[-1]
+    folder_name = file_name.split(".")[0]
 
-        put(archive_path, "/tmp/{}".format(file_name))
-        run("mkdir -p /data/web_static/releases/{}/".format(folder_name))
-        #exttrackt the tar file
-        run("tar -xzf /tmp/{} -C \
-             /data/web_static/releases/{}/".format(file_name, folder_name))
+    put(archive_path, "/tmp/{}".format(file_name))
+    run("mkdir -p /data/web_static/releases/{}/".format(folder_name))
+    #exttrackt the tar file
+    run("tar -xzf /tmp/{} -C \
+         /data/web_static/releases/{}/".format(file_name, folder_name))
 
-        run("rm /tmp/{}".format(file_name))
-        run("cp -r /data/web_static/releases/{}/web_static/* \
-             /data/web_static/releases/{}/".format(folder_name, folder_name))
+    run("rm /tmp/{}".format(file_name))
+    run("cp -r /data/web_static/releases/{}/web_static/* \
+         /data/web_static/releases/{}/".format(folder_name, folder_name))
 
-        run("rm -fr \
-             /data/web_static/releases/{}/web_static/".format(folder_name))
+    run("rm -fr \
+         /data/web_static/releases/{}/web_static/".format(folder_name))
 
-        run("rm -f /data/web_static/current")
-        run("ln -sf /data/web_static/releases/{}/ \
-             /data/web_static/current".format(folder_name))
+    run("rm -f /data/web_static/current")
+    run("ln -sf /data/web_static/releases/{}/ \
+         /data/web_static/current".format(folder_name))
 
-        run("chmod -R 755 /data/")
+    run("chmod -R 755 /data/")
 
-        print("New version deployed!")
+    print("New version deployed!")
 
 
 def deploy():

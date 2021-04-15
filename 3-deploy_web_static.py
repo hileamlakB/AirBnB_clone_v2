a747e77364604870074b8ce7c3db46a57f7c3c47
hileamlakb <hilea.buisness@gmail.com>
2021-04-15 15:50:58 +0300

    Complette deployment scripts

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,10 +7,32 @@ Clone repo, using the function do_pack
 from fabric.api import *
 from datetime import datetime
 
+env.arc_src = './web_static'
+env.arc_name = 'web_static_.tgz'
 env.hosts = ['34.74.94.56', '34.74.11.212']
 # User name and password should prferably be based
 # as environmnt variables rather than explicitly typing them
 # here 
+
+def do_pack():
+    """ Packages soruce code into a compressed archive
+    """
+    global env
+
+    try:
+
+        now = datetime.now()
+        tstr = now.strftime("%Y%m%d%H%M%S")
+        env.arc_name = 'web_static_{}.tgz'.format(tstr)
+
+        print("Packing web_static to versions/{}".format(env.arc_name))
+        
+        loc3-deploy_web_static.pyal("if test ! -d ./versions; then mkdir versions;fi")
+        local("tar -cvzf versions/%s %s" % (env.arc_name, env.arc_src), capture=False)
+        return env.arc_src+"/versions/{}".format(env.arc_name)
+    except:
+        return None
+
 
 def do_deploy(archive_path):
     """Deploys a achive to a number of remote hosts
@@ -37,4 +59,12 @@ def do_deploy(archive_path):
         run("chmod -R 755 /data/")
 
         print("New version deployed!")
-        
+
+def deploy():
+    """Packages and deployes source code
+    """
+
+    archive_path = do_pack()
+    if not archive_path:
+        return False
+    return do_deploy(archive_path)
